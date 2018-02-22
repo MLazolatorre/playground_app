d2873f3584d98cf610e4d39c16fbb2af5897a197
MarcLazo <lazolatorre@et.esiea.fr>
2018-02-22 18:15:38 +0100

    WallCallengeList fonctional. Missing redux

--- src/components/wallChallengeList/WallChallengeDetail.js
+++ src/components/wallChallengeList/WallChallengeDetail.js
@@ -10,12 +10,6 @@ import {
   Card,
 } from '../common';
 
-import PictureProfile1 from '../../../img/devTest/PictureProfile1.jpg';
-import PictureProfile2 from '../../../img/devTest/PictureProfile2.jpg';
-import ChallDescription1 from '../../../img/devTest/ChallDescription1.jpg';
-import ChallDescription2 from '../../../img/devTest/ChallDescription2.jpg';
-
-
 class WallCallengeDetail extends Component {
   constructor(prop) {
     super(prop);
@@ -23,12 +17,12 @@ class WallCallengeDetail extends Component {
   }
 
   render() {
-    const headerSource = players.map(x => ({
+    const headerSource = this.props.data.map(x => ({
       name: x.name,
       profilePicture: x.profilePicture,
     }));
 
-    const bodySources = players.map(x => ({
+    const bodySources = this.props.data.map(x => ({
       nbLikes: x.nbLikes,
       challengeDescription: x.challengeDescription,
     }));
@@ -48,20 +42,6 @@ class WallCallengeDetail extends Component {
     );
   }
 }
-
-const players = [
-  {
-    name: 'Méloch',
-    profilePicture: PictureProfile1,
-    challengeDescription: ChallDescription1,
-    nbLikes: 180564,
-  }, {
-    name: 'Macron',
-    profilePicture: PictureProfile2,
-    challengeDescription: ChallDescription2,
-    nbLikes: 204564,
-  }
-];
 
 const styles = StyleSheet.create({
   card: {
--- src/components/wallChallengeList/WallChallengeList.js
+++ src/components/wallChallengeList/WallChallengeList.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import {
+  FlatList,
   View,
 } from 'react-native';
 
 import WallCallengeDetail from './WallChallengeDetail';
+
+
+import PictureProfile1 from '../../../img/devTest/PictureProfile1.jpg';
+import PictureProfile2 from '../../../img/devTest/PictureProfile2.jpg';
+import ChallDescription1 from '../../../img/devTest/ChallDescription1.jpg';
+import ChallDescription2 from '../../../img/devTest/ChallDescription2.jpg';
+
 
 class WallCallengeList extends Component {
   constructor(prop) {
@@ -11,13 +19,52 @@ class WallCallengeList extends Component {
     this.state = {};
   }
 
+  keyExtractor(item, index) {
+    return `${index}`;
+  }
+
+  renderItem({ item }) {
+    console.log('item');
+    console.log(item);
+
+    return (
+      <WallCallengeDetail
+        data={item}
+      />
+    );
+  }
+
   render() {
+    let data = [];
+
+    for (let i = 0; i < 30; i++) {
+      data = [...data, players];
+    }
+
     return (
       <View>
-        <WallCallengeDetail />
+        <FlatList
+          data={data}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+        />
       </View>
     );
   }
 }
+
+const players = [
+  {
+    name: 'Méloch',
+    profilePicture: PictureProfile1,
+    challengeDescription: ChallDescription1,
+    nbLikes: 180564,
+  }, {
+    name: 'Macron',
+    profilePicture: PictureProfile2,
+    challengeDescription: ChallDescription2,
+    nbLikes: 204564,
+  }
+];
 
 export default WallCallengeList;
